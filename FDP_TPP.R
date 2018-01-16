5bc2d929511c26b6e5ce9a8007f49c8c9f575ccb
matgug <mathieu.guglielmino@gmail.com>
2018-01-16 19:26:41 +0100

    first false discovery / last true selection

--- FDP_TPP.R
+++ FDP_TPP.R
@@ -1,33 +1,65 @@
 library(glmnet)
 
-data(QuickStartExample)
-
-beta = c(rep(4, 200), rep(0, 800))
-
-X = matrix( rnorm(1010 * 1000), nrow=1010, ncol=1000)
-y = X%*%beta
-
-lasso <- glmnet(X, y, lambda = 0.1)
-k = 200
 
 TPP <- function(model, beta, k) {
+  # True positive proportion
   coefs <- coef.glmnet(model)@i
   common_coefs <- intersect(which(beta > 0), coefs)
   length(common_coefs)/max(k, 1)
 }
 
 FDP <- function(model, beta, k) {
+  # False discoveries proportion
   coefs <- coef.glmnet(model)@i
   not_common_coefs <- intersect(which(beta == 0), coefs)
   length(not_common_coefs)/max(length(coefs), 1)
 }
 
-FDP_TPP <- function(lambda, X, y, k, beta) {
+TPP_FDP <- function(lambda, X, y, k, beta) {
+  # (TPP, FDP)
+  # lambda = 0, tous les coefficients sont sélectionnés
   model <- glmnet(X, y, lambda = lambda)
   TPP <- TPP(model, beta, k)
   FDP <- FDP(model, beta, k)
   c(TPP, FDP)
 }
 
-plot(t(sapply(seq(0.1, 50, length.out = 1000), function(x) FDP_TPP(x, X, y, k, beta) )),
-     xlab = 'TPP', ylab = 'FDP', main = 'Lasso path', cex=0.5, col="blue")
+first_false_selection <- function(k, beta) {
+  # When the first false selection happens
+  # Generate data under same gaussian random design setting
+  X = matrix( rnorm(1010 * 1000), nrow=1010, ncol=1000)
+  y = X%*%beta
+  
+  lambda <- 20
+  tpp_fdp <- TPP_FDP(lambda, X, y, k, beta)
+  tpp <- tpp_fdp[1]
+  fdp <- tpp_fdp[2]
+  i <- 1
+  while (fdp == 0) {
+    lambda <- lambda - 0.5
+    tpp_fdp <- TPP_FDP(lambda, X, y, k, beta)
+    tpp <- tpp_fdp[1]
+    fdp <- tpp_fdp[2]
+  }
+  c(tpp, fdp, lambda)
+}
+
+last_true_selection <- function(k, beta) {
+  # When the last true selection happens
+  # Generate data under same gaussian random design setting
+  X = matrix( rnorm(1010 * 1000), nrow=1010, ncol=1000)
+  y = X%*%beta
+  
+  lambda <- 0.1
+  tpp_fdp <- TPP_FDP(lambda, X, y, k, beta)
+  tpp <- tpp_fdp[1]
+  fdp <- tpp_fdp[2]
+  i <- 1
+  while (tpp == 1) {
+    lambda <- lambda + 0.5
+    tpp_fdp <- TPP_FDP(lambda, X, y, k, beta)
+    tpp <- tpp_fdp[1]
+    fdp <- tpp_fdp[2]
+  }
+  c(tpp, fdp, lambda)
+}
---
+++ results.R
@@ -0,0 +1,28 @@
+source('FDP_TPP.R')
+# Data generation under gaussian random design
+beta = c(rep(4, 200), rep(0, 800))
+
+X = matrix( rnorm(1010 * 1000), nrow=1010, ncol=1000)
+y = X%*%beta
+k = 200
+
+# Lasso path
+lasso_path <- t(sapply(seq(0.1, 50, length.out = 1000), function(x) FDP_TPP(x, X, y, k, beta) ))
+pdf('lasso_path1.pdf', width=6, height=4)
+plot(lasso_path,
+     xlab = 'TPP', ylab = 'FDP', main = 'Lasso path', cex=0.5, col="deepskyblue3")
+dev.off()
+
+# Histogram of the TPP at time of first false selection
+tpp_first_false_selection <- sapply(1:100, function(x) first_false_selection(k, beta)[1])
+# Histogram of the FDP at time of last true selection
+fdp_last_true_selection <- sapply(1:100, function(x) last_true_selection(k, beta)[2])
+
+#png('hist_tpp_first_false_fdp_last_true.png', width=900, height=500)
+pdf('hist_tpp_first_false_fdp_last_true.pdf', width=12, height=6)
+par(mfrow=c(1,2))
+hist(tpp_first_false_selection, col = "gold2",
+     xlab = "TPP at time of first false selection", main="")
+hist(fdp_last_true_selection, col = "gold2",
+     xlab = "TPP at time of first false selection", main="")
+dev.off()
